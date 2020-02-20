9aec5f0ec287f60a57747003be11d7a9a1cac20d
roshan <roshan.jayathunga@colombo.rezgateway.com>
2020-02-20 18:36:22 +0530

    Updates

---
+++ precache-manifest.06ef638512b094a954713cb4a8c3c8fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995566003aabd5d243198bc95e8926f8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2859f8ea1d1cb0648255",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "651907a7e02234f7cfcc",
+    "url": "/react-gh-pages/static/js/2.33af256b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-gh-pages/static/js/2.33af256b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2859f8ea1d1cb0648255",
+    "url": "/react-gh-pages/static/js/main.d6a69cd4.chunk.js"
+  },
+  {
+    "revision": "ecfd1d276f9241e07c49",
+    "url": "/react-gh-pages/static/js/runtime-main.d24436ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
